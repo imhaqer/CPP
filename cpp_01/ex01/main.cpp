5ff1f29311aa1f09a604c3350fd1585f54d4c707
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-07-04 14:45:18 +0300

    called destructor manually

--- cpp_01/ex01/main.cpp
+++ cpp_01/ex01/main.cpp
@@ -6,20 +6,23 @@ int main()
 	Zombie *horde;
 	int N = 5;
 
-	horde = zombieHorde(N, "Horde");
+	horde = zombieHorde(N, "zombieHorde");
 
 	for (int i = 0; i < N; i++)
 	{
 		//horde[i] = horde->announce();
-		std::cout <<"Index " << i << ": ";
-		horde[i].announce();
+		// std::cout <<"Index " << i << ": ";
+		// horde[i].announce();
+
+		std::cout << "[" << i << "]"; horde[i].announce();
+		// std::cout << "Zombie " << i << " address: " << &horde[i] << "\n";
 		
 	}
-
-
-
-
-
+	// delete[] horde;
+	for (int i = 0; i < N; i++) {
+        horde[i].~Zombie();
+    }
+ 	operator delete[](horde);
 
 	return (0);
 }
