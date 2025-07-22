a7c9d04ff924ed399d86dec049ac17a0867a6cb6
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-07-22 16:41:28 +0300

    done

--- cpp_02/ex00/Fixed.hpp
+++ cpp_02/ex00/Fixed.hpp
@@ -1,22 +1,20 @@
 #ifndef FIXED_HPP
-#define FIXED_HPP
+# define FIXED_HPP
 
-#include <iostream>
+# include <iostream>
 
 class Fixed {
-    private:
-        int _rawValue;
-        static const int _fractionalBits;
+  private:
+	int _rawValue;
+	static const int _fractionalBits = 8;
 
-    public:
-        Fixed();
-        ~Fixed();
-        Fixed(const Fixed& other);
-        Fixed& operator=(const Fixed& obj);
-        int getRawBits() const;
-        void setRawBits(const int raw);
-
+  public:
+	Fixed();
+	~Fixed();
+	Fixed(const Fixed &other);
+	Fixed &operator=(const Fixed &obj);
+	int getRawBits() const;
+	void setRawBits(const int raw);
 };
-
 
 #endif
--- cpp_02/ex01/Fixed.hpp
+++ cpp_02/ex01/Fixed.hpp
@@ -1,26 +1,27 @@
 #ifndef FIXED_HPP
-#define FIXED_HPP
+# define FIXED_HPP
 
-#include <iostream>
-#include <cmath>
+# include <cmath>
+# include <iostream>
 
 class Fixed {
-    private:
-        int _rawValue;
-        static const int _fractionalBits;
+  private:
+	int _rawValue;
+	static const int _fractionalBits = 8;
 
-    public:
-        Fixed();
-        ~Fixed();
-        Fixed(const int value);
-        Fixed(const float value);
-        Fixed(const Fixed& other);
-        float toFloat() const;
-        int toInt() const;
-        Fixed& operator=(const Fixed& obj);
-        int getRawBits() const;
-        void setRawBits(const int raw);
+  public:
+	Fixed();
+	~Fixed();
+	Fixed(const int value);
+	Fixed(const float value);
+	Fixed(const Fixed &other);
 
+	float toFloat() const;
+	int toInt() const;
+	
+	Fixed &operator=(const Fixed &obj);
+	int getRawBits() const;
+	void setRawBits(const int raw);
 };
 
 std::ostream &operator<<(std::ostream &stream, const Fixed &nbr);
