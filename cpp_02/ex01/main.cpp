d5a61a4119d8ac132277fdbd368abe51a779bce2
Hager Hamdan <93748219+imhaqer@users.noreply.github.com>
2025-07-22 16:55:28 +0300

    done

--- cpp_02/ex01/main.cpp
+++ cpp_02/ex01/main.cpp
@@ -7,13 +7,17 @@ a;
 Fixed const b( 10 );
 Fixed const c( 42.42f );
 Fixed const d( b );
+Fixed const z( 54.73f );
+
 a = Fixed( 1234.4321f );
+
 
 
 std::cout << "a is " << a << std::endl;
 std::cout << "b is " << b << std::endl;
 std::cout << "c is " << c << std::endl;
 std::cout << "d is " << d << std::endl;
+std::cout << "z is " << z << std::endl;
 
 return 0;
 }
